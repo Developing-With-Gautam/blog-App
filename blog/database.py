480efcbc36f72680f6a15b7c8a8d729b1fdcb72c
Gautam Yadav <gautam.yadav@nexxbase.com>
2025-10-04 14:10:51 +0530

    dockerfile added

--- blog/database.py
+++ blog/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/blog"
+DATABASE_URL = "postgresql://postgres:postgres@db:5432/blog"
 
 engine = create_engine(DATABASE_URL)
 
